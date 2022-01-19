01b1a4c887e97b08e09c9112f5a4dbba277addc0
kingcarzy <53007334+kingcarzy@users.noreply.github.com>
2022-01-19 13:47:33 +0800

    Create TestTaintStep.java

---
+++ src/main/java/com/l4yn3/microserviceseclab/data/TestTaintStep.java
@@ -0,0 +1,11 @@
+package com.jus4fun.xyz;
+
+public class TestTaintStep {
+    String name;
+    public void setName(String input){
+        this.name = input;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
